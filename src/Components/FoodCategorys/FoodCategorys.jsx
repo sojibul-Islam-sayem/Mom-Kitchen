99a86f0c6b56acd8babc1b4f1b9e8b3ada1108f5
sayemp2 <sojibulislamsayem420@gmail.com>
2025-01-22 23:31:36 +0600

    updated: food Categorys

---
+++ src/Components/FoodCategorys/FoodCategorys.jsx
@@ -0,0 +1,49 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import category1 from '../../assets/home/slide1.jpg'
+import category2 from '../../assets/home/slide2.jpg'
+import category3 from '../../assets/home/slide3.jpg'
+import category4 from '../../assets/home/slide4.jpg'
+import category5 from '../../assets/home/slide5.jpg'
+
+const FoodCategorys = () => {
+    return (
+        <div>
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper mb-10"
+            >
+                <SwiperSlide >
+                    <img src={category1} alt="" />
+                    <h3 className='text-4xl uppercase -mt-20 text-center text-white'>salads</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={category2} alt="" />
+                    <h3 className='text-4xl uppercase -mt-20 text-center text-white'>pizzas</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={category3} alt="" />
+                    <h3 className='text-4xl uppercase -mt-20 text-center text-white'>Soups</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={category4} alt="" />
+                    <h3 className='text-4xl uppercase -mt-20 text-center text-white'>desserts</h3>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={category5} alt="" />
+                    <h3 className='text-4xl uppercase -mt-20  text-center text-white'>salads</h3>
+                </SwiperSlide>
+
+            </Swiper>
+        </div>
+    );
+};
+
+export default FoodCategorys;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from '../../Components/Banner/Banner';
+import FoodCategorys from '../../Components/FoodCategorys/FoodCategorys';
 
 
 
@@ -8,6 +9,7 @@ const Home = () => {
     return (
         <div>
            <Banner></Banner>
+           <FoodCategorys></FoodCategorys>
         </div>
     );
 };
