23265d33bc4a20f5f74480525f479765fe76b2a5
sayemp2 <sojibulislamsayem420@gmail.com>
2025-01-22 23:07:28 +0600

    updated: 2nd commit

--- src/Components/SharedComponets/Navbar/Navbar.jsx
+++ src/Components/SharedComponets/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
     </>
 
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar max-w-7xl mx-auto fixed z-30 bg-opacity-20 bg-black text-white">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -37,14 +37,14 @@ const Navbar = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                       {NavItem}
+                        {NavItem}
                     </ul>
                 </div>
                 <a className="btn btn-ghost text-xl">Sem's Foods</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-{NavItem}
+                    {NavItem}
                 </ul>
             </div>
             <div className="navbar-end">
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Banner from '../../Components/Banner/Banner';
+
+
+
 
 const Home = () => {
     return (
         <div>
-            Home
+           <Banner></Banner>
         </div>
     );
 };
