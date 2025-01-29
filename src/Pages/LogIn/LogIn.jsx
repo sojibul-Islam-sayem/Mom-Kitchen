42fc8dd6ca0000cd424f0ff93319743316f71296
sayemp2 <sojibulislamsayem420@gmail.com>
2025-01-29 23:12:30 +0600

    updated

--- src/Pages/LogIn/LogIn.jsx
+++ src/Pages/LogIn/LogIn.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import img from "../../assets/others/authentication2.png"
 import { Link } from 'react-router-dom';
+import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
 
 const LogIn = () => {
+    const captchaRef = useRef(null);
+    const [disable, setDisable] = useState(true)
+    useEffect(() => {
+        loadCaptchaEnginge(6);
+    }, [])
+
 
     const handleSignIn = e => {
         e.preventDefault();
@@ -10,7 +17,16 @@ const LogIn = () => {
         const email = form.email.value;
         const password = form.password.value;
         console.log(email, password);
+    }
 
+    const validCaptcha = () => {
+        const value = captchaRef.current.value;
+        if (validateCaptcha(value)) {
+            setDisable(false)
+        }
+        else {
+            setDisable(true)
+        }
     }
     return (
         <div className="min-h-screen ">
@@ -18,7 +34,7 @@ const LogIn = () => {
                 <div className="w-1/2">
                     <img src={img} alt="" />
                 </div>
-                <form onSubmit={handleSignIn} className="w-1/2">
+                <form onSubmit={handleSignIn} className="w-1/2 flex flex-col space-y-4">
                     <h3 className='text-4xl text-center font-bold text-black'>Sign In</h3>
                     <div>
                         <label className='block text-xl font-semibold mb-2 text-black' htmlFor="">Email</label>
@@ -28,7 +44,11 @@ const LogIn = () => {
                         <label className='block text-xl font-semibold mb-2 text-black' htmlFor="">Password</label>
                         <input className='bg-white w-full py-3 border-2 rounded-md pl-4' placeholder='Enter your Password' type="password" name="password" id="" />
                     </div>
-                    <input className='bg-[#D1A054] w-full font-bold rounded-md text-center py-5 text-xl text-white' type="submit" value="Sign In" />
+                    <div>
+                        <label className='block text-xl font-semibold mb-2 text-black' htmlFor=""><LoadCanvasTemplate /></label>
+                        <input ref={captchaRef} onBlur={validCaptcha} className='bg-white w-full py-3 border-2 rounded-md pl-4' placeholder='Enter the Captcha' type="captcha" name="captcha" id="" />
+                    </div>
+                    <input disabled={disable} className={`btn bg-[#D1A054] border-0 w-full font-bold rounded-md text-center  text-xl text-white`} type="submit" value="Sign In" />
                     <p className='text-center text-xl text-[#D1A054]'>New here? <Link className='font-bold'>Create a New Account</Link></p>
                     <p className='text-center text-[#444444] text-xl'>Or sign in with</p>
                 </form>
