ff814bfdf91249d5d3b0562d65adb880b3d0a3c4
sayemp2 <sojibulislamsayem420@gmail.com>
2025-02-02 23:26:01 +0600

    updated

--- src/Components/SharedComponets/FoodCard/FoodCard.jsx
+++ src/Components/SharedComponets/FoodCard/FoodCard.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import useAuth from '../../../Hooks/useAuth';
+import Swal from 'sweetalert2'
+import { useNavigate } from 'react-router-dom';
 
 const FoodCard = ({ item }) => {
     const { name, recipe, price, image } = item;
+    const {user} = useAuth()
+    const navigate = useNavigate()
 
     const handleCart = food => {
-        console.log(food);
+        if(user && user.email){
+
+        }
+        else{
+            Swal.fire({
+                title: "Please Login",
+                text: "Please Login to add to the cart",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "LogIn"
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  navigate('/login')
+                }
+              });   
+        }
+          
     }
 
     return (
